26c4eb60ca59c7e4552fdc62d6cb4cced94335f3
btate712 <btate712@gmail.com>
2020-03-17 19:05:37 -0500

    wrote spec to test get route

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -12,17 +12,40 @@ module ExpenseTracker
     end 
 
     it 'records submitted expenses' do
-      coffee = {
-        'payee' => 'Starbucks',
-        'amount' => 5.75,
-        'date' => '2017-06-10'
-      }
+      
+      def post_expense(expense)
+        coffee = post_expense(
+          'payee' => 'Starbucks',
+          'amount' => 5.75,
+          'date' => '2017-06-10'
+          )
+          
+        zoo = post_expense(
+          'payee' => 'Zoo',
+          'amount' => 15.25,
+          'date' => '2017-06-10'
+        )
+      
+        groceries = post_expense(
+          'payee' => 'Whole Foods',
+          'amount' => 95.20,
+          'date' => '2017-06-101'
+        )
 
-      post '/expenses', JSON.generate(coffee)
+        post '/expenses', JSON.generate(expense)
+        expect(last_response.status).to eq(200)
+
+        parsed = JSON.parse(last_response.body)
+        expect(parsed).to include('expense_id' => a_kind_of(Integer))
+        expect.merge('id' => parsed['expense_id'])
+      end
+
+      get 'expenses/2017-06-10'
       expect(last_response.status).to eq(200)
 
-      parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
+
     end
   end
 end
